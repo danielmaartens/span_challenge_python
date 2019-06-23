b488d0f637a54354b842ca35f1a3d3503ff01715
Daniel Maartens <daniel@touchfoundry.co.za>
2019-06-23 21:19:27 +0200

    Completed main.py to have one function called run

--- challenge/main.py
+++ challenge/main.py
@@ -1,119 +1,36 @@
-import re
-import operator
-from challenge.team_value import TeamValue
-from challenge.utils.convert import dictionary_to_team_value_list, dictionary_to_list
+import sys
 
-file = "/Users/danielmaartens/personal/span/span_challenge_python/challenge/resources/input.csv"
-
-list = []
-
-dictionary = {}
-
-team_result_grouping_pattern = "^([a-zA-Z\\s]+)([0-9]+$)"
-
-with open(file) as f:
-    content = f.readlines()
-
-content = [line.strip() for line in content]
-
-
-def calculate_match_points(match_results):
-    match_points = []
-
-    team_a = match_results[0]
-    team_b = match_results[1]
-
-    team_a_name = team_a.getname()
-    team_a_goals = team_a.getvalue()
-    team_a_points = TeamValue(team_a_name, 0)
-
-    team_b_name = team_b.getname()
-    team_b_goals = team_b.getvalue()
-    team_b_points = TeamValue(team_b_name, 0)
-
-    if team_a_goals == team_b_goals:
-        team_a_points.setvalue(1)
-        team_b_points.setvalue(1)
-    elif team_a_goals > team_b_goals:
-        team_a_points.setvalue(3)
-    else:
-        team_b_points.setvalue(3)
-
-    match_points.append(team_a_points)
-    match_points.append(team_b_points)
-
-    return match_points
-
-
-def reduce_team_match_points(all_teams_match_points):
-    final_team_points = {}
-
-    for team in all_teams_match_points:
-        name = team.getname()
-        points = team.getvalue()
-
-        if name in final_team_points:
-            next_points_total = final_team_points[name] + points
-            final_team_points[name] = next_points_total
-        else:
-            final_team_points[name] = points
-
-    return dictionary_to_list(final_team_points)
-
-
-def set_team_rank(sorted_team_match_points):
-    ranked_teams = []
-
-    index = 1
-    rank = 0
-    previous_team_points = None
-
-    for team_match_point in sorted_team_match_points:
-        name = team_match_point[0]
-        points = team_match_point[1]
-
-        if points != previous_team_points:
-            rank += 1
-
-        this_team_rank = rank
-
-        if points == previous_team_points:
-            rank = index
-
-        team = TeamValue(name, points)
-        team.setrank(this_team_rank)
-        ranked_teams.append(team)
-
-        previous_team_points = points
-        index += 1
-
-    return ranked_teams
+from challenge.utils import *
 
 
 def run():
-    team_match_points = []
+    running = True
 
-    for x in content:
-        y = x.split(', ')
-        match_outcome = []
-        for xy in y:
-            r = re.search(team_result_grouping_pattern, xy)
-            if r:
-                # remove space from name
-                name = r.group(1)[:-1]
-                result = int(r.group(2))
-                team_value = TeamValue(name, result)
-                match_outcome.append(team_value)
+    delay = 1.5
 
-        match_points = calculate_match_points(match_outcome)
-        team_match_points.extend(match_points)
+    print('\nWelcome to the Match Point Calculator !\n')
 
-    final_team_match_points = reduce_team_match_points(team_match_points)
+    delayed_print('This program will calculate the ranking table for a soccer league.\n', delay)
 
-    final_team_match_points.sort(key=operator.itemgetter(0))
-    final_team_match_points.sort(key=operator.itemgetter(1), reverse=True)
+    delayed_print('The data for the results of the games should be stored in a text file.', delay)
 
-    ranked_teams = set_team_rank(final_team_match_points)
+    while running:
 
-    for team in ranked_teams:
-        print(str(team.getrank()) + '. ' + team.getname() + ', ' + str(team.getvalue()) + (' pt' if team.getvalue() == 1 else ' pts'))
+        delayed_print('\nPlease provide the full path of the file where your results are stored:\n', delay)
+
+        file_path = input('Full File Path: ')
+
+        ranked_teams = get_ordered_match_points_from_file(file_path)
+
+        print('\nRESULTS\n')
+
+        for team in ranked_teams:
+            print(str(team.getrank()) + '. ' + team.getname() + ', ' + str(team.getvalue()) + (
+                ' pt' if team.getvalue() == 1 else ' pts'))
+
+        user_carry_on = input('\nWould you like to check match point results of another league ? [Y/N]: ')
+
+        running = boolean_from_string(user_carry_on)
+
+    print('\nThank you for using the Match Point Calculator !')
+    sys.exit()
